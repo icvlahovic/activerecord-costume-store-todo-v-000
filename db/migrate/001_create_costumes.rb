a57df55c8a2acbef5dc994b341a5a0b7a9448acc
Ian Vlahovic <banddorkfht7@gmail.com>
2018-06-01 08:09:02 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,5 +9,12 @@
 # value types according to the spec
 class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.text :image_url
+      t.string :size
+      t.timestamps
+    end
   end
 end
